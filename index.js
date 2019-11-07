c4cc45d8d849a0cda7b512e053605f7552f1e4d8
Pankush Gupta <pankush.gupta@gmail.com>
2019-11-07 22:40:11 +0530

    basic setup files

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-let express = require('express');
+const express = require('express');
+const socket = require('socket.io');
+
+const port = 3000;
+
+let app = express();
+
+let server = app.listen(port,()=>{
+    console.log(`Listening at port ${port}`)
+});
+
+app.use(express.static('public'));
+
+let io = socket(server);
+
+io.on('connection',(socket)=>{
+    console.log('Socket Connection Established : ' + socket.id);
+   
+    socket.on('chat',(data)=>{
+        io.sockets.emit('chat', data);
+    });
+});
