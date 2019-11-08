7c748f2d6cd85a492c0c4a8763bb31463d2e540b
Pankush Gupta <pankush.gupta@gmail.com>
2019-11-08 21:56:14 +0530

    minor

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ app.use(express.static('public'));
 let io = socket(server);
 
 io.on('connection',(socket)=>{
-    console.log('Socket Connection Established : ' + socket.id);
+    console.log('Socket Connection Established : '+socket.id);
+    socket.join(['room1'],(err)=>{
+        if(err) console.log(err);
+        else console.log(`room joined`);
+    })
    
     socket.on('chat',(data)=>{
         io.sockets.emit('chat', data);
