2593d601fc2985361cf94876323dc5ab5e971426
Pankush Gupta <pankush.gupta@gmail.com>
2019-11-09 00:10:21 +0530

    chat functionality through angular and socket io

--- Momento/src/app/services/socket.service.ts
+++ Momento/src/app/services/socket.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
-import io from 'socket.io-client';
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
 
-  constructor() { }
+  socket : any;
+  readonly uri:string='http://localhost:3000';
+
+  constructor() { 
+    this.socket = io(this.uri);
+   }
+
+  listen(eventName:string){
+    return new Observable(subscriber=>{
+      this.socket.on(eventName,(data)=>{
+        subscriber.next(data);
+      });
+    });
+  }
+
+  emit(eventName:string, data){
+    this.socket.emit(eventName,data)
+  }
 }
